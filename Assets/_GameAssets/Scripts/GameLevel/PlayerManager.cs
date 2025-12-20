c8d6691f299c7a4e99c9c403222dd048fb77aff8
DreamBit Game <103414856+MustafaYldz00@users.noreply.github.com>
2025-12-20 14:55:40 +0300

    v0.7 Player Manager Added

--- Assets/_GameAssets/Scripts/GameLevel/PlayerManager.cs
+++ Assets/_GameAssets/Scripts/GameLevel/PlayerManager.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform _gun;
 
-    // Update is called once per frame
+    float _angle;
+    float _speed = 5f;
+
     void Update()
     {
-        
+       RotateChange();
+    }
+
+    public void RotateChange()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - _gun.transform.position;
+            _angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+
+        }
+
+
+        Quaternion rotation = Quaternion.AngleAxis(_angle, Vector3.forward);
+        _gun.transform.rotation = Quaternion.Slerp(_gun.transform.rotation, rotation, _speed*Time.deltaTime);
     }
 }
